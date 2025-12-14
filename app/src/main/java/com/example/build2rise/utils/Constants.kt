b537a413b8fd4636aaede5b3448a58b328673bd8
evezhinin <evezhinin@gmail.com>
2025-12-13 19:51:55 -0500

    updated API base URL to point to production server

--- app/src/main/java/com/example/build2rise/utils/Constants.kt
+++ app/src/main/java/com/example/build2rise/utils/Constants.kt
@@ -2,7 +2,7 @@ package com.example.build2rise.utils
 
 object Constants {
     // For Android Emulator
-    const val BASE_URL = "http://10.0.2.2:8080/api/"
+    const val BASE_URL = "https://csci380-build2rise-server.onrender.com/api/"
 
     // Preferences
     const val PREFS_NAME = "build2rise_prefs"
